68324d58ddaea20ba17180a3de36b31c9d31b6fd
Onur Basar <66836980+onurbasar@users.noreply.github.com>
2022-02-09 13:53:44 +0300

    remove comments

--- lambda.tf
+++ lambda.tf
@@ -14,9 +14,6 @@ resource "aws_lambda_function" "test-lambda-with-terraform" {
   role          = aws_iam_role.lambda_role_s3.arn
   handler       = "${var.PROJECT}.lambda_handler"
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   source_code_hash = filebase64sha256(local.lambda_zip_path)
   runtime = "python3.8"
 
